aba5497cc44966e065c30daeaa1b2fe71f904c37
Armeddon <84802373+Armeddon@users.noreply.github.com>
2021-12-07 21:43:54 +0300

    Add files via upload

---
+++ Deep_Labeling/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Deep_Labeling project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Deep_Labeling.settings')
+
+application = get_asgi_application()
