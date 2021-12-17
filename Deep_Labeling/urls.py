23091d4785d6c42d83b732a8b209f6bf6ee0a861
Armeddon <84802373+Armeddon@users.noreply.github.com>
2021-12-17 18:45:52 +0300

    Update urls.py

--- Deep_Labeling/urls.py
+++ Deep_Labeling/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Frontend import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home),
+    path('editor.html', views.editor)
 ]
+
+urlpatterns += staticfiles_urlpatterns()
