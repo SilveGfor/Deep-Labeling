d167d1ce64feee03c5a88a09a7edb4c252f97994
SilveGfor <63398117+SilveGfor@users.noreply.github.com>
2022-02-06 17:26:08 +0300

    settings

--- Deep_Labeling/settings.py
+++ Deep_Labeling/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'Deep_Labeling.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [os.path.join(BASE_DIR, 'Frontend/Webpages')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
