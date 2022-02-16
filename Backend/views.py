634382560bf89d7bff32ea154ca05c6906805777
Armeddon <84802373+Armeddon@users.noreply.github.com>
2022-02-16 22:08:10 +0300

    Update views.py

--- Backend/views.py
+++ Backend/views.py
@@ -5,6 +5,8 @@ from rest_framework.views import APIView
 from rest_framework import generics, status
 from django.http import HttpResponse
 
+import json
+
 from Backend.serializers import UsersSerializer
 
 from Backend.models import User, Project
@@ -14,9 +16,10 @@ class Authorization(APIView):
 
     def post(self, request):
         #print(request.data)
-        user_email = request.data.get('email')
-        user_password = request.data.get('password')
-        user = User.objects.get(email=user_email)
+        data = json.loads(request.body.decode('utf-8'))
+        user_email = data['email']
+        user_password = data['password']
+        user = User.objects.filter(email=user_email).first()
         users = User.objects.all()
         #print(users)
 
@@ -24,9 +27,9 @@ class Authorization(APIView):
         #print(str(project))
 
         serializer = UsersSerializer(data=request.data)
-        if serializer.is_valid():
-            if user.password == user_password:
-                return Response({"status": "success"})
-            else:
-                return Response({"status": "incorrect_password"})
+        #if serializer.is_valid():
+        if user.password == user_password:
+            return Response({"status": "success"})
+        else:
+            return Response({"status": "incorrect_password"})
         return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
